6ec3c66c1b39d0da3ce5af6e0c2f0a11749a02ac
Jørgen Jacobsen <jorgen.jacobsen@aller.com>
2018-09-25 11:09:56 +0200

    added carousel

--- tasks/config/copy.js
+++ tasks/config/copy.js
@@ -6,17 +6,19 @@ module.exports = function (grunt, pkg){
                     files: [
                         { expand: true, dot: true, cwd: './', src: ['manifest.json', '.htaccess', 'data/*.json', 'fonts/{,*/}*.{woff,woff2,ttf,eot}'], dest: '<%= config.dest %>/' },
                         { expand: true, dot: true, cwd: 'assets/favicon/styleguide/dev/', src: ['favicon.ico'], dest: '<%= config.dest %>/' },
-                        { expand: true, dot: true, cwd: 'assets/', src: ['**/*.{png,jpg,gif,svg}'], dest: '<%= config.dest %>/assets/' }
+                        { expand: true, dot: true, cwd: 'assets/', src: ['**/*.{png,jpg,gif,svg,js}'], dest: '<%= config.dest %>/assets/' }
                     ]
                 },
                 dist: {
                     files: [
+                        
                         { expand: true, dot: true, cwd: './', src: ['manifest.json', 'styleguide.html', '.htaccess','data/*.json', 'fonts/{,*/}*.{woff,woff2,ttf,eot}'], dest: '<%= config.dest %>/' },
                         { expand: true, dot: true, cwd: 'assets/', src: ['**/*.svg'], dest: '<%= config.dest %>/assets/' },
                         { expand: true, dot: true, cwd: 'assets/favicon/styleguide/prod/', src: ['favicon.ico'], dest: '<%= config.dest %>/' },
                         { expand: true, cwd: '<%= config.dest %>/', src: ['scripts/**', 'fonts/**', 'assets/**'], dest: '<%= config.dest %>/<%= config.version.major %>/' },
                         { expand: true, cwd: '<%= config.dest %>/', src: ['scripts/**', 'fonts/**', 'assets/**'], dest: '<%= config.dest %>/<%= config.version.minor %>/' },
-                        { expand: true, cwd: '<%= config.dest %>/', src: ['scripts/**', 'fonts/**', 'assets/**'], dest: '<%= config.dest %>/<%= config.version.patch %>/' }
+                        { expand: true, cwd: '<%= config.dest %>/', src: ['scripts/**', 'fonts/**', 'assets/**'], dest: '<%= config.dest %>/<%= config.version.patch %>/' },
+                        { expand: true, dot: true, cwd: 'assets/', src: ['**/*.js'], dest: '<%= config.dest %>/assets/' }
                     ]
                 }
             }
