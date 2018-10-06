16a4ab3a4f1a10fd210ab86aed5570e6489ffbe1
Jørgen Jacobsen <jorgeja@gmail.com>
2018-10-06 11:16:02 +0200

    updated carousel with new script

--- assets/kode24-scripts/carousel.js
+++ assets/kode24-scripts/carousel.js
@@ -1,3 +1,86 @@
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]]; // eslint-disable-line no-param-reassign
+  }
+}
+
+$(function () {
+  var ads = [];
+  var autoJobcarousel = $(".auto-job-carousel");
+  $.ajax(
+    {
+      type : 'GET',
+      url : "http://api.dagbladet.no/article/?query=section:jobb&site_id=207",
+      headers : {
+      'Access-Control-Allow-Origin' : '*'
+      },
+      success: function (data) {
+        var ads = data.result.filter(ad => ad.visibility_status !== "H");
+        shuffleArray(ads);
+        adNodesToCarousel(ads);        
+      }
+    }
+  );
+
+
+
+  function adNodesToCarousel (ads) {
+    console.log(ads);
+    let adsContainer = $('<div class="ads-wrapper"></div>');
+    ads.forEach(function (ad) {
+      adsContainer.append(`
+        <article id="${ad.id}">
+          <a itemprop="url" href="${ad.published_url}">
+            <figure class="image-contain" style="background-image: url(//dbstatic.no/${ad.image}.jpg?imageid=${ad.image}&height=300&compression=80)">
+            </figure>
+            <div class="article-preview-text">
+              <h1 class="headline">
+                ${ad.title}
+              </h1>
+              <p class="standfirst">${ad.subtitle}</p>
+              <div class="labels">
+                <span class="label label-custom">${ad.full_bylines[0].firstname} ${ad.full_bylines[0].lastname}</span>
+              </div>
+            </div>
+          </a>
+        </article>
+      `);
+    });
+
+    autoJobcarousel.append(adsContainer);
+    autoJobcarousel.append(`
+      <a href="/jobb/" class="more-jobs"><span>Se alle ledige stillinger</span></a>
+    `)
+
+    adsContainer.slick({
+      infinite: true,
+      speed: 300,
+      slidesToShow: 3,
+      centerMode: true,
+      arrows: true,
+      accessibility: true,
+      responsive: [
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            arrows: false
+          }
+        }
+      ]
+    });
+  }
+  
+});
+
+
+
+
+
+
 $(function () {
 
   var rows = $(".frontpage .row");
