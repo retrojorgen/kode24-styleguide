efb0e2b92eaa52583efb6007acffae3f19dc51e1
Jørgen Jacobsen <jorgeja@gmail.com>
2019-01-15 20:49:24 +0100

    track outbound links script

---
+++ assets/kode24-scripts/outboundAdTracking.js
@@ -0,0 +1,29 @@
+// tracks clicks on outboundlinks
+
+
+<script>
+/**
+* Funksjon som sporer klikk på en utgående link i Analytics.
+* Denne funksjonen tar en gyldig nettadressestreng som argument og bruker denne strengen
+* som aktivitetsetikett. Hvis transportmetoden angis som «beacon», kan treffet sendes
+* med «navigator.sendBeacon» i nettlesere som støtter dette.
+*/
+var trackOutboundLink = function(url) {
+   ga('send', 'event', 'ekstern_lenke', 'click', url, {
+     'transport': 'beacon',
+     'hitCallback': function(){document.location = url;}
+   })
+}
+</script>
+
+<script>
+  $(function () {
+    $("a").click(function (event) {
+      let targetUrl = event.currentTarget.href;
+      if(targetUrl.indexOf("https://www.kode24.no/") < 0) {
+        trackOutboundLink(targetUrl);
+        console.log("Went to", targetUrl);
+      }
+    })
+  })
+</script>
