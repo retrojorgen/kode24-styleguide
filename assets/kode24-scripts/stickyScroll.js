d34112d0d8dffe5f24e6613d0bac40124886929f
Jørgen Jacobsen <jorgeja@gmail.com>
2019-04-29 12:09:56 +0200

    add right away

--- assets/kode24-scripts/stickyScroll.js
+++ assets/kode24-scripts/stickyScroll.js
@@ -2,11 +2,20 @@
 if (window.location.pathname !== "/") {
   var headerNavigation = document.getElementById("top-navigation");
   var headerCloack = document.getElementById("header-cloak");
+  var alwaysSticky = false;
+
+  if (!document.querySelector("article header .full-bleed")) {
+    console.log("sticky");
+    headerNavigation.classList.add("sticky");
+    headerCloack.classList.add("sticky");
+    alwaysSticky = true;
+  }
+
   window.onscroll = function() {
     if (window.pageYOffset > 0) {
       headerNavigation.classList.add("sticky");
       headerCloack.classList.add("sticky");
-    } else {
+    } else if (alwaysSticky) {
       headerNavigation.classList.remove("sticky");
       headerCloack.classList.remove("sticky");
     }
