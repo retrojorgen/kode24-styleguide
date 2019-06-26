7314f52b4ad77f3e2f47d5a446df37d7ac654dab
Jørgen Jacobsen <jorgeja@gmail.com>
2019-06-26 10:10:34 +0200

    ny toppbanner

--- assets/kode24-scripts/topBanner.js
+++ assets/kode24-scripts/topBanner.js
@@ -2,17 +2,16 @@ $(() => {
   function randomNumber(max) {
     return Math.floor(Math.random() * max + 0);
   }
-  /**
 
   let main = $("main");
-  let desktopAds = ["https://www.dagbladet.no/files/2019/06/09/980x300.png"];
-  let mobileAds = ["https://www.dagbladet.no/files/2019/06/09/980x300.png"];
+  let desktopAds = ["https://www.dagbladet.no/files/2019/06/26/toppbanner-kodere.jpg"];
+  let mobileAds = ["https://www.dagbladet.no/files/2019/06/26/toppbanner-kodere.jpg"];
   let desktopAd = desktopAds[randomNumber(desktopAds.length - 1)];
   let mobileAd = mobileAds[randomNumber(desktopAds.length - 1)];
 
   let ad = $(`
     <div class="row top-profile" style="margin-top: 20px;">
-      <a itemprop="url" href="https://ndcoslo.com/" target="_blank">
+      <a itemprop="url" href="https://www.kode24.no/annonse/priser-pa-annonser-og-content-pa-kode24/70244826" target="_blank">
           <div class="kicker">ANNONSE</div> 
           <figure class="desktop">
               <img itemprop="image" alt="annonse" src="${desktopAd}">
@@ -26,5 +25,4 @@ $(() => {
   `);
 
   main.before(ad);
-   */
 });
